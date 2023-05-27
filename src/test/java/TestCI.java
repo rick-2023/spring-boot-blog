1948a9c065c6813ed7878e08beb6dbff381f2077
zrfan@leqee.com <zrfan@leqee.com>
2023-05-27 16:47:29 +0800

    feat:ci test

---
+++ src/test/java/TestCI.java
@@ -0,0 +1,15 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author zrfan
+ * @date 2023/05/27.
+ */
+public class TestCI {
+
+  @Test
+  public void testName() {
+    Assert.assertEquals(1, 1);
+  }
+
+}
